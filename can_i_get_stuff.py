ea2e0157ea1f198e5aa8142f331c2bd88fa14e68
Matt Bennett <matt.bennett91@gmail.com>
2015-11-06 16:18:05 +1300

    omg commit fast conflicts!

---
+++ can_i_get_stuff.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+class TracksResourceLocation:
+  layer = 753
+  max_results = 1
+  key = '2342af39f38841248750a72eb5a1a0c5'
+  radius = 20000  
+
+  def __init__(self, lat, long):
+    self.x = long
+    self.y = lat
+
+  def getUri(self):
+    return 'http://api.koordinates.com/api/vectorQuery.json/'
+
+  def getRequestParams(self):
+    return {
+            'layer': self.layer, 
+            'max_results': self.max_results,
+            'key': self.key,
+            'radius': self.radius, 
+            'x': self.x, 
+            'y': self.y,
+            'geometry': 'false'
+    }
+
+class Track:
+  def __init__(self, name, lat, long):
+    self.name = name
+    self.lat = lat
+    self.long = long
+
+  def asJson(self):
+    return json.dumps(self.__dict__)
+
+t = Track("Matiu", 123, 345)
+print t.asJson()
+
+#41.2889 S, 174.7772 E is wellington
+loc = TracksResourceLocation(-41.2889, 174.7772)
+r = requests.get(loc.getUri(), loc.getRequestParams())
+print "response: %s" % r.status_code
+track = r.json()
+print track['vectorQuery']['layers']['753']['features'][0]['properties']['DESCRIPTION']

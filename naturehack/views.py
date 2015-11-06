df138ba8145010b12c9d21390ed4961087217e05
Matt Bennett <matt.bennett91@gmail.com>
2015-11-06 18:43:01 +1300

    return tracks as proper json

--- naturehack/views.py
+++ naturehack/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 import requests
+import json
 from can_i_get_stuff import TracksResourceLocation, TracksBuilder
 
 def homepage(request):
     loc = TracksResourceLocation(-41.2889, 174.7772)
     r = requests.get(loc.getUri(), loc.getRequestParams())
     ts = TracksBuilder(r.json()).getTracks()
-    return HttpResponse(ts)
+    jsonList = [t.asJson() for t in ts]
+    return HttpResponse(json.dumps(jsonList))
 
 

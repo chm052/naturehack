dfb27d1aaac22482b0cfacbfa9fc6e6613e34554
Liam Roth-Thomas <liam.roth-thomas@xero.com>
2015-11-07 21:51:37 +1300

    :tropical_fish:Put everything in index.html because static files and django wut?

--- naturehack/templates/naturehack/frontend/index.js
+++ naturehack/templates/naturehack/frontend/index.js
@@ -33,9 +33,11 @@ function createWalks(walks) {
 	walks.forEach(function(walk) {
 		item = doc.createElement('li');
 		item.classList.add('hikr__walks-item');
-		item.innerHtml = walk.name;
+		item.innerHTML = walk.name;
 		hikrWalkList.appendChild(item);
 	});
 
 	return hikrWalkList;
 }
+
+var mock = [{lat: 174.8656943777268, name: "CIRCUIT TRACK MATIU", long: -41.25547727765185}, {lat: 174.86416062697066, name: "Road to Circuit Track", long: -41.25831435774681}, {lat: 174.8660373791062, name: "Southern Summit Track", long: -41.25963291993241}, {lat: 174.86567057352957, name: "Workshop to VC Track", long: -41.256484419270315}, {lat: 174.86512994666623, name: "Memorial Track", long: -41.25474276763705}, {lat: 174.86650207849337, name: "BULLOCK TRACK", long: -41.2559556336113}, {lat: 174.86744419896112, name: "DEGAUSSING STATION TRACK", long: -41.256334556932664}, {lat: 174.70736657777832, name: "Makara Carpark to Gun Emplacements", long: -41.218407491659924}, {lat: 174.6947224810965, name: "MAKARA MERIDIAN ROAD SECTION", long: -41.23012942670318}, {lat: 174.68940575482617, name: "Makara Road End to Opau Stream mouth", long: -41.233250561610774}];

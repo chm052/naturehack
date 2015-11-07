06598785babf64c167747e08b1cb3cb9dbbb2057
Liam Roth-Thomas <liam.roth-thomas@xero.com>
2015-11-07 17:54:56 +1300

    :hamburger: Add index.html and index.js with basic xhr setup

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// var url = 'index.html/sub';
+var url = 'endpoint.html';
+
+uxhr(url, '', {
+		method: 'GET',
+		complete: complete,
+		success: success,
+		error: error
+	});
+
+	function complete (response, status) {
+		console.log('request complete', status);
+	}
+
+	function success (response) {
+		console.log('"' + response + '"');
+	}
+
+	function error (response, status) {
+		console.log('"' + response + '"', status);
+	}

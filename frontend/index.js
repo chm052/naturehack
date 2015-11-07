7cb34f248b46249afbe2ba771257eef61b94d62a
Liam Roth-Thomas <liam.roth-thomas@xero.com>
2015-11-07 20:27:26 +1300

    :boar: commit all the things

--- frontend/index.js
+++ frontend/index.js
@@ -1,22 +1,41 @@
 'use strict';
 
-var url = '/naturehack';
+// Get the data
+var url = '../naturehack';
 
 uxhr(url, '', {
-		method: 'GET',
-		complete: complete,
-		success: success,
-		error: error
+	method: 'GET',
+	complete: complete,
+	success: success,
+	error: error
+});
+
+function complete (response, status) {
+	console.log('request complete', status);
+}
+
+function success (response) {
+	console.log('"' + response + '"');
+	createWalks(response);
+}
+
+function error (response, status) {
+	console.log('"' + response + '"', status);
+}
+
+function createWalks(walks) {
+	var doc = document;
+	var item;
+	var hikrWalkList = doc.createElement('ul');
+
+	hikrWalkList.classList.add('hikr__walks');
+
+	walks.forEach(function(walk) {
+		item = doc.createElement('li');
+		item.classList.add('hikr__walks-item');
+		item.innerHtml = walk.name;
+		hikrWalkList.appendChild(item);
 	});
 
-	function complete (response, status) {
-		console.log('request complete', status);
-	}
-
-	function success (response) {
-		console.log('"' + response + '"');
-	}
-
-	function error (response, status) {
-		console.log('"' + response + '"', status);
-	}
+	return hikrWalkList;
+}
